72a9630ed80fb314bfcd9866d54b9f8f536aa923
Lisa <lisapothoven@gmail.com>
2019-12-03 08:57:55 +0100

    Changes to classification.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import sys
 
-sys.path.append('./Music-Emotion-Recognition2/')
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+    
+sys.path.append('./Music-Emotion-Recognition/')
 
 featEx = __import__('Feature-Extraction')
 
